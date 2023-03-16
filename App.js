0774c9ff7baaa76e41bdb25cea110390861bcb12
GuadalupFranco <franco.ramirez.guadalup@gmail.com>
2023-03-16 17:44:09 -0600

    add background light for status-bar and use of Platform

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Main from './src/components/Main.jsx'
 import { NativeRouter } from 'react-router-native';
+import { StatusBar } from 'expo-status-bar'
 
 export default function App() {
-  return (
+  return <>
+    <StatusBar style='light' />
     <NativeRouter>
       <Main />
     </NativeRouter>
-  )
+  </>
 }
