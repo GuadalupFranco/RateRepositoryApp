158764458c82e0bafb05f344755a572900ae8ab3
GuadalupFranco <franco.ramirez.guadalup@gmail.com>
2023-04-10 15:34:25 -0600

    add login form validation with Yup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { Formik, useField } from 'formik'
 import StyledTextInput from '../components/StyledTextInput.jsx'
 import StyledText from '../components/StyledText.jsx'
+import { loginValidationSchema } from '../validationSchemas/login.js'
 
 const initialValues = {
 	email: '',
@@ -39,22 +40,9 @@ const FormikInputValue = ({ name, ...props }) => {
 	)
 }
 
-const validate = values => {
-	const errors = {}
-
-	if(!values.email) {
-		errors.email = 'Email is required'
-	} else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-		errors.email = 'Invalid email address'
-	}
-
-	console.log(errors)
-
-	return errors
-}
 export default function LogInPage() {
 	return (
-		<Formik validate={validate} initialValues={initialValues} onSubmit=
+		<Formik validationSchema={loginValidationSchema} initialValues={initialValues} onSubmit=
 			{ values => console.log(values) }>
 			{({ handleSubmit }) => {
 				return (
